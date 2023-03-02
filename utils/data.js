90df8754c53291fc93510494084eaa00c39d23f5
dmytro.kashka <dmytro.kashka@beetroot.se>
2023-03-02 15:01:36 +0200

    product list

---
+++ utils/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'Free Shirt',
+      slug: 'free-shirt',
+      category: 'Shirts',
+      image: '/images/shirt1.jpg',
+      price: 70,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt'
+    },
+    {
+      name: 'Free Shirt 1',
+      slug: 'free-shirt',
+      category: 'Shirts',
+      image: '/images/shirt2.jpg',
+      price: 50,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt'
+    },
+    {
+      name: 'Free Shirt 2',
+      slug: 'free-shirt',
+      category: 'Shirts',
+      image: '/images/shirt3.jpg',
+      price: 20,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 8,
+      countInStock: 20,
+      description: 'A popular shirt'
+    },
+  ]
+}
+
+export default data
